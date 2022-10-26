4e7bcf29772e0c3bb30cdcc62f7c5f97085639ab
Souhila YERMECHE <yermsouhila@gmail.com>
2022-10-26 16:10:41 +0200

    Ajout de la page 404

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Home} from './pages/home/Home'
 import {Fiche} from './pages/fiche/Fiche'
 import {A_propos} from './pages/a_propos/A_propos'
 import {Pages} from './pages/pages'
+import {NotFound} from './components/NotFound'
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,7 +19,8 @@ function App() {
           <Route path="/" element= {<Home title=""/>}></Route>
           <Route path="/fiche" element= {<Fiche title=""/>}></Route>
           <Route path="/A_propos" element= {<A_propos title=""/>}></Route>
-        </Route> 
+        </Route>
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </Router>
   );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export const NotFound = () => {
+    return(
+        <div>
+            <h1>Hey I dont exist !</h1>
+        </div>
+        );
+}
