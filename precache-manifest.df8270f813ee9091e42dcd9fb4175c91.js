699e44c852c849fbf50b98b0a9a19c94a932ba2e
Souhila YERMECHE <yermsouhila@gmail.com>
2022-11-07 15:10:58 +0100

    Updates

---
+++ precache-manifest.df8270f813ee9091e42dcd9fb4175c91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b48b5bcd47e6cf20bac8",
+    "url": "/kasa/static/css/main.1b652875.chunk.css"
+  },
+  {
+    "revision": "b48b5bcd47e6cf20bac8",
+    "url": "/kasa/static/js/main.b48b5bcd.chunk.js"
+  },
+  {
+    "revision": "2b447e1be3e7dd9deafc",
+    "url": "/kasa/static/js/1.2b447e1b.chunk.js"
+  },
+  {
+    "revision": "67c02b910aca3547887e",
+    "url": "/kasa/static/js/2.67c02b91.chunk.js"
+  },
+  {
+    "revision": "5b8e21f919604951eaf9",
+    "url": "/kasa/static/js/runtime~main.5b8e21f9.js"
+  },
+  {
+    "revision": "3d1174c05282982d68e7d36607b71d0b",
+    "url": "/kasa/static/media/IMG.3d1174c0.svg"
+  },
+  {
+    "revision": "a7e59686d2c1d507777d00a705c1b25b",
+    "url": "/kasa/static/media/Simage.a7e59686.svg"
+  },
+  {
+    "revision": "889ecb9dcb066e540135c75101ef6f77",
+    "url": "/kasa/static/media/.LOGO.889ecb9d.svg"
+  },
+  {
+    "revision": "318727ec6ceb182a5ea06a90ecc0aaee",
+    "url": "/kasa/static/media/.LOGO.318727ec.svg"
+  },
+  {
+    "revision": "7fcfd6b31e316a63e3d2f8b25c45988c",
+    "url": "/kasa/static/media/notfound.7fcfd6b3.svg"
+  },
+  {
+    "revision": "c795ffea40905233bf15565680fafac3",
+    "url": "/kasa/index.html"
+  }
+];
