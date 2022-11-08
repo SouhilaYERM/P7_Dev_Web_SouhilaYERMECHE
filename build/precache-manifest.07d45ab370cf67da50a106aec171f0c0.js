4c4302be1b20510bf935856243734007e0ae8b4e
Souhila YERMECHE <yermsouhila@gmail.com>
2022-11-08 12:03:46 +0100

    ajout du build

---
+++ build/precache-manifest.07d45ab370cf67da50a106aec171f0c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "94ec60113784d62969c5",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/css/main.70c4c4ab.chunk.css"
+  },
+  {
+    "revision": "94ec60113784d62969c5",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/js/main.94ec6011.chunk.js"
+  },
+  {
+    "revision": "2b447e1be3e7dd9deafc",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/js/1.2b447e1b.chunk.js"
+  },
+  {
+    "revision": "81fe2f60dd3bdcd2b52d",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/js/2.81fe2f60.chunk.js"
+  },
+  {
+    "revision": "bad08793d997e58503b4",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/js/runtime~main.bad08793.js"
+  },
+  {
+    "revision": "3d1174c05282982d68e7d36607b71d0b",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/media/IMG.3d1174c0.svg"
+  },
+  {
+    "revision": "a7e59686d2c1d507777d00a705c1b25b",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/media/Simage.a7e59686.svg"
+  },
+  {
+    "revision": "889ecb9dcb066e540135c75101ef6f77",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/media/.LOGO.889ecb9d.svg"
+  },
+  {
+    "revision": "318727ec6ceb182a5ea06a90ecc0aaee",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/media/.LOGO.318727ec.svg"
+  },
+  {
+    "revision": "7fcfd6b31e316a63e3d2f8b25c45988c",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/static/media/notfound.7fcfd6b3.svg"
+  },
+  {
+    "revision": "c4a966d04f768cc58f9b84b47d2d4838",
+    "url": "/P7_Dev_Web_SouhilaYERMECHE/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/P7_Dev_Web_SouhilaYERMECHE/precache-manifest.07d45ab370cf67da50a106aec171f0c0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/P7_Dev_Web_SouhilaYERMECHE/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
